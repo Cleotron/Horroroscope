79fa1d2c7928e6d4abbbb20ad056afbe9ebea43b
CLEOTRON <HIDALGO.ANA@GMAIL.COM>
2017-10-21 01:26:23 +0200

    Añadir texto

--- MyBot.java
+++ MyBot.java
@@ -19,14 +19,14 @@ public class MyBot{
 
         //send a tweet
         String [] allSigns = new String[] {"Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"};
-        String [] allVerbs = new String[] {"hit", "bitten", "smashed", "drowned in pigeonpoop", "burnt", "stabbed", "beheaded", "squashed", "impaled", "bled to death", "chopped", "infected", "shot", "run over", "hit on the head", "poisoned", "cut your veins open", "forced to drink champu"};
-        String [] allCompl = new String[] {" by a furious monkey", " by a lovely kitty", " by my little pony", " by Cthulhu", " by the Kraken", " with a rusty axe", " with your dog's leash", " with a hammer", " by your neighbour", " by a mutant bee", " by a hungry koala", " by a crossed eyed spider" };
+        String [] allVerbs = new String[] {"be hit", " be bitten", "be smashed", "be drowned in pigeonpoop", "be burnt", "be stabbed", "be beheaded", "be squashed", "be impaled", "be bled to death", "be chopped", "be infected", "be shot", "be run over", "be hit on the head", "be poisoned", "have your veins cut open", "be forced to drink shampoo", "be skinned", "be cut into julienne strips", "be punched to death", "be eye-gouged", "be buried alive", "have your throat slit", "be eviscerated", "have your nails ripped off", "have a vivisection done", "be cooked", "be cremated", "be experimented on", "be eaten alive", "be forced to cut your own feet", "be pushed into a black hole"};
+        String [] allCompl = new String[] {" by a furious monkey", " by a lovely kitty", " by my little pony", " by Cthulhu", " by the Kraken", " with a rusty axe", " with your dog's leash", " with a hammer", " by your neighbour", " by a mutant bee", " by a hungry koala", " by a crossed eyed spider", "by Son Goku", "with a potato peeler", "by your coworker", "in the dumpster", "under the sea", "in the subway", "by a cab driver", "in a family meeting", "by a haunted doll", "in some basement", "with a letter opener", "by a Teletubbie", "on TV", "in a reality show", "by aliens"};
 
         while(true){
           for(String sign: allSigns){
             String verb = (allVerbs[new Random().nextInt(allVerbs.length)]);
             String complement = (allCompl[new Random().nextInt(allCompl.length)]);
-            Status status = twitter.updateStatus("Dear " + sign + ", today you will be " + verb + complement);
+            Status status = twitter.updateStatus("Dear " + sign + ", today you will " + verb + complement);
             System.out.println("Done.");
             Thread.sleep(60*60*1000);
           }
